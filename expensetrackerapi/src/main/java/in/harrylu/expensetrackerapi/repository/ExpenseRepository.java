2c57ab6265db358b51dadba6ed408c51fece7c51
haixiaolu <haixiao.lu-1@ou.edu>
2022-07-10 13:45:23 -0500

    Added Mapping between Expenses and Users

--- expensetrackerapi/src/main/java/in/harrylu/expensetrackerapi/repository/ExpenseRepository.java
+++ expensetrackerapi/src/main/java/in/harrylu/expensetrackerapi/repository/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package in.harrylu.expensetrackerapi.repository;
 
 import java.sql.Date;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,12 +12,20 @@ import in.harrylu.expensetrackerapi.entity.Expense;
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 	
-	//SELECT * FROM tbl_expenses WHERE category = ?
-	Page <Expense>findByCategory(String category, Pageable page);
-
-	//SELECT * FROM tbl_expenses WHERE name LIKE %keyword%
-	Page <Expense>findByNameContaining(String keyword, Pageable page);
 	
-	//SELECT * FROM tbl_expense WHERE date BETWEEN 'startDate' AND 'endDate'
-	Page <Expense>findByDateBetween(Date startDate, Date endDate, Pageable page);
+	Page<Expense> findByUserIdAndCategory(Long userId, String category, Pageable page);
+
+	
+	Page<Expense> findByUserIdAndNameContaining(Long userId,String keyword, Pageable page);
+	
+	
+	Page<Expense> findByUserIdAndDateBetween(Long userId, Date startDate, Date endDate, Pageable page);
+	
+	
+	Page<Expense> findByUserId(Long userId, Pageable page);
+	
+	
+	Optional<Expense> findByUserIdAndId(Long userId, Long expenseId);
+	
+
 }
--- expensetrackerapi/src/main/java/in/harrylu/expensetrackerapi/service/ExpenseServiceImpl.java
+++ expensetrackerapi/src/main/java/in/harrylu/expensetrackerapi/service/ExpenseServiceImpl.java
@@ -20,14 +20,17 @@ public class ExpenseServiceImpl implements ExpenseService {
 	@Autowired
 	private ExpenseRepository expenseRepo;
 	
+	@Autowired
+	private UserService userService;
+	
 	@Override
 	public Page<Expense> getAllExpenses(Pageable page) {
-		return expenseRepo.findAll(page);
+		return expenseRepo.findByUserId(userService.getLoggedInUser().getId(), page);
 	}
 
 	@Override
 	public Expense getExpenseById(Long id){
-		Optional<Expense> expense = expenseRepo.findById(id);
+		Optional<Expense> expense = expenseRepo.findByUserIdAndId(userService.getLoggedInUser().getId(), id);
 		if (expense.isPresent()) {
 			return expense.get();
 		}
@@ -42,6 +45,7 @@ public class ExpenseServiceImpl implements ExpenseService {
 
 	@Override
 	public Expense saveExpenseDetails(Expense expense) {
+		expense.setUser(userService.getLoggedInUser());
 		return expenseRepo.save(expense);
 	}
 
@@ -58,12 +62,12 @@ public class ExpenseServiceImpl implements ExpenseService {
 
 	@Override
 	public List<Expense> readByCategory(String category, Pageable page) {
-		return expenseRepo.findByCategory(category, page).toList();
+		return expenseRepo.findByUserIdAndCategory(userService.getLoggedInUser().getId(), category, page).toList();
 	}
 
 	@Override
 	public List<Expense> readByName(String keyword, Pageable page) {
-		return expenseRepo.findByNameContaining(keyword, page).toList();
+		return expenseRepo.findByUserIdAndNameContaining(userService.getLoggedInUser().getId(), keyword, page).toList();
 	}
 
 	@Override
@@ -77,7 +81,7 @@ public class ExpenseServiceImpl implements ExpenseService {
 			endDate = new Date(System.currentTimeMillis());
 		}
 		
-		return expenseRepo.findByDateBetween(startDate, endDate, page).toList();
+		return expenseRepo.findByUserIdAndDateBetween(userService.getLoggedInUser().getId(), startDate, endDate, page).toList();
 	}
 
 }
